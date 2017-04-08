b48c1a14cfa7876ca88965ac5db90fa916d5daa2
Kjell Vos <vos.kjell@gmail.com>
2017-04-08 21:12:19 +0200

    Started working on the register part

--- src/com/kjellvos/school/kassaSystem/MainMenu.java
+++ src/com/kjellvos/school/kassaSystem/MainMenu.java
@@ -2,19 +2,27 @@ package com.kjellvos.school.kassaSystem;
 
 import com.kjellvos.os.gridHandler.GridHandler;
 import com.kjellvos.school.kassaSystem.common.Extensions.MainScene;
+import com.kjellvos.school.kassaSystem.common.database.Categorie;
+import com.kjellvos.school.kassaSystem.common.database.Item;
 import com.kjellvos.school.kassaSystem.common.interfaces.SceneImplementation;
+import com.kjellvos.school.kassaSystem.database.Database;
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TitledPane;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.*;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.sql.SQLException;
 
 /**
  * Created by kjell on 10-3-2017.
  */
 public class MainMenu extends MainScene implements SceneImplementation {
+    private Database database;
+
     private Scene scene;
 
     private Button productenButton, bonnenButton, kaartenButton, kassaLadeButton, corrigeerButton, betaalButton;
@@ -25,9 +33,24 @@ public class MainMenu extends MainScene implements SceneImplementation {
 
     public MainMenu(Stage stage) {
         super(stage);
+        try {
+            database = new Database(this);
+        } catch (SQLException e) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Whoops!");
+            alert.setHeaderText("Er lijkt geen database te zijn!");
+            alert.setContentText("Als er wel databasse hoort te zijn, vraag dan uw administrator om hulp!");
+            alert.getDialogPane().getChildren().stream().filter(node -> node instanceof Label).forEach(node -> ((Label)node).setMinHeight(Region.USE_PREF_SIZE));
+            alert.showAndWait();
+
+            Platform.exit();
+            e.printStackTrace();
+        }
+
         super.getPrimaryStage().setTitle("Kassa V0.1");
         super.getPrimaryStage().setWidth(800D);
         super.getPrimaryStage().setHeight(600D);
+        super.getPrimaryStage().setResizable(false);
         super.changeScene(this);
         super.getPrimaryStage().show();
     }
@@ -42,16 +65,20 @@ public class MainMenu extends MainScene implements SceneImplementation {
         corrigeerButton = new Button("Corrigeer");
         betaalButton = new Button("Betaal");
 
-        AnchorPane pane = new AnchorPane();
-        TitledPane categorie1 = new TitledPane("Categorie 1", new Pane());
-        pane.setTopAnchor(categorie1, 0D);
-        pane.setBottomAnchor(categorie1, 0D);
-        pane.setLeftAnchor(categorie1, 0D);
-        pane.setRightAnchor(categorie1, 0D);
-        pane.getChildren().add(categorie1);
+        ObservableList categories = database.getCategories();
+        TitledPane[] titledPanes = new TitledPane[categories.size()];
+        Accordion accordion = new Accordion();
+        for (int i = 0; i < categories.size(); i++) {
+            ObservableList items = database.getItemsByCategorie(((Categorie)categories.get(i)).getId());
+            VBox vBox = new VBox();
+            for (int j = 0; j < items.size(); j++) {
+                Text text = new Text(((Item)items.get(j)).getPrice());
+                vBox.getChildren().add(text);
+            }
+            titledPanes[i] = new TitledPane(((Categorie)categories.get(i)).getName(), vBox);
+        }
+        accordion.getPanes().addAll(titledPanes);
 
-        categorieListView = new TitledPane("Categorieen", pane);
-        categorieListView.setCollapsible(false);
         bonListView = new ListView();
 
         gridHandler.add(0,0, productenButton, false);
@@ -59,7 +86,7 @@ public class MainMenu extends MainScene implements SceneImplementation {
         gridHandler.add(0, 2, kaartenButton, false);
         gridHandler.add(0, 4, kassaLadeButton, false);
 
-        gridHandler.add(1, 0, categorieListView, 1, 5, false);
+        gridHandler.add(1, 0, accordion, 1, 5, false);
 
         gridHandler.add(2, 0, bonListView, 1, 3, false);
         gridHandler.add(2, 3, corrigeerButton, false);
@@ -68,6 +95,8 @@ public class MainMenu extends MainScene implements SceneImplementation {
         scene = gridHandler.getGridAsScene();
         return scene;
     }
+
+
 
     @Override
     public Scene getScene() {
--- src/com/kjellvos/school/kassaSystem/database/Database.java
+++ src/com/kjellvos/school/kassaSystem/database/Database.java
@@ -1,35 +1,163 @@
 package com.kjellvos.school.kassaSystem.database;
 
 import com.kjellvos.school.kassaSystem.Main;
+import com.kjellvos.school.kassaSystem.MainMenu;
+import com.kjellvos.school.kassaSystem.common.Extensions.DatabaseExt;
+import com.kjellvos.school.kassaSystem.common.database.Categorie;
+import com.kjellvos.school.kassaSystem.common.database.Item;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Region;
 import org.apache.commons.dbcp2.BasicDataSource;
 
-import javax.naming.Context;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.io.InputStream;
+import java.sql.*;
 
 /**
  * Created by kjevo on 3/29/17.
  */
 @SuppressWarnings("Duplicates")
-public class Database {
-    Main main;
-    BasicDataSource basicDataSource;
-    Connection connection;
-    PreparedStatement preparedStatement;
+public class Database extends DatabaseExt{
+    MainMenu mainMenu;
 
-    public Database(Main main) throws SQLException {
-        this.main = main;
-
-        System.setProperty(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.fscontext.RefFSContextFactory");
-        System.setProperty(Context.PROVIDER_URL, "file:///tmp");
-
-        basicDataSource = new BasicDataSource();
-        basicDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        basicDataSource.setUsername("KassaSystem");
-        basicDataSource.setPassword("password123321");
-        basicDataSource.setUrl("jdbc:mysql://213.154.224.189/KassaSystem?useSSL=false&reconnect=true&allowMultiQueries=true");
-
-        connection = basicDataSource.getConnection();
+    public Database(MainMenu mainMenu) throws SQLException {
+        super();
+        this.mainMenu = mainMenu;
     }
+
+    public ObservableList getItemsByCategorie(int passedId){
+        ObservableList<Item> data = FXCollections.observableArrayList();
+        try{
+            super.setConnection(super.getBasicDataSource().getConnection());
+
+            String sql =    "SELECT Items.ID, Items.Name, Items.Description, DefaultPrices.Price, Categories.Name AS CName, ItemsImages.Image FROM Items \n" +
+                            "   LEFT JOIN DefaultPrices ON Items.ID = DefaultPrices.ItemsID\n" +
+                            "   LEFT JOIN ItemsImages ON Items.ID = ItemsImages.ItemsId\n" +
+                            "   LEFT JOIN CategorieItems ON CategorieItems.ItemsId=Items.ID\n" +
+                            "   LEFT JOIN Categories ON Categories.ID=CategorieItems.CategorieId\n" +
+                            "   WHERE Categories.ID=?;";
+            super.setPreparedStatement(super.getConnection().prepareStatement(sql));
+            super.getPreparedStatement().setInt(1, passedId);
+            ResultSet resultSet = super.getPreparedStatement().executeQuery();
+            if (resultSet.next()) {
+                resultSet.beforeFirst();
+                while (resultSet.next()){
+                    int id = resultSet.getInt("ID");
+                    String name = resultSet.getString("Name");
+                    String description = resultSet.getString("Description");
+                    String price = resultSet.getString("Price");
+                    String categorie = resultSet.getString("CName");
+                    Blob imageBlob = resultSet.getBlob("Image");
+                    InputStream imageInputStream = imageBlob.getBinaryStream();
+                    Image image = new Image(imageInputStream);
+
+                    sql = "SELECT Price FROM Prices WHERE FromWhen > NOW() AND NOW() < TillWhen AND ItemsID=?;";
+                    super.setPreparedStatement(super.getConnection().prepareStatement(sql));
+                    super.getPreparedStatement().setInt(1, id);
+                    ResultSet resultSet1 = super.getPreparedStatement().executeQuery();
+                    while (resultSet1.next()){
+                        price = resultSet1.getString("Price");
+                    }
+
+                    data.add(new Item(id, name, description, price, categorie, image));
+                }
+            }else{
+                showNoDataAlert();
+                System.out.println("Error 2");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                super.getPreparedStatement().close();
+                super.getConnection().close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return data;
+    }
+
+    public ObservableList getCategories() {
+        ObservableList<Categorie> data = FXCollections.observableArrayList();
+        try{
+            super.setConnection(super.getBasicDataSource().getConnection());
+
+            String sql =    "SELECT * FROM Categories;";
+            super.setPreparedStatement(super.getConnection().prepareStatement(sql));
+            ResultSet resultSet = super.getPreparedStatement().executeQuery();
+            if (resultSet.next()) {
+                resultSet.beforeFirst();
+                while (resultSet.next()){
+                    int id = resultSet.getInt("ID");
+                    String name = resultSet.getString("Name");
+
+                    data.add(new Categorie(id, name));
+                }
+            }else{
+                showNoDataAlert();
+                System.out.println("Error 1");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                super.getPreparedStatement().close();
+                super.getConnection().close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return data;
+    }
+
+    private void showSuccesfullyUpdated() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Succesvol geupdate!");
+        alert.setHeaderText("De waarden zijn succesvol aangepast!");
+        alert.setContentText("Hij heeft nu de nieuwe waarden!");
+        alert.showAndWait();
+    }
+
+    public void showOopsAlert(){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Whoops!");
+        alert.setHeaderText("Er ging iets royaal mis!");
+        alert.setContentText("Vraag uw administrator om hulp!");
+        alert.getDialogPane().getChildren().stream().filter(node -> node instanceof Label).forEach(node -> ((Label)node).setMinHeight(Region.USE_PREF_SIZE));
+
+        alert.showAndWait();
+    }
+
+    public void showNoDataAlert(){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Whoops!");
+        alert.setHeaderText("Er lijkt geen data te zijn!");
+        alert.setContentText("Als er wel data hoort te zijn, vraag dan uw administrator om hulp!");
+        alert.getDialogPane().getChildren().stream().filter(node -> node instanceof Label).forEach(node -> ((Label)node).setMinHeight(Region.USE_PREF_SIZE));
+
+        alert.showAndWait();
+    }
+
+    public void showSuccesfullyUploaded(){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Geupload!");
+        alert.setHeaderText("Succesvol geupload!");
+        alert.setContentText("Het item is succesbvol geupload!");
+        alert.showAndWait();
+    }
+
+    private void showSuccesfullyDeleted() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Verwijderd!");
+        alert.setHeaderText("Succesvol verwijderd!");
+        alert.setContentText("Het item is succesvol verwijderd!");
+        alert.showAndWait();
+    }
+
 }
